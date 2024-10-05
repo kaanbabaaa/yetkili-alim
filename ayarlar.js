b296e66b3d44be6168a91ebde030a37312a089cd
kaanbabaaa <kaantorunkaan11@gmail.com>
2024-10-04 20:27:49 -0700

    Update ayarlar.js

--- ayarlar.js
+++ ayarlar.js
@@ -1,4 +1,4 @@
 module.exports = {
-    token: '', 
-    sahipID: '',
+    token: 'MTI5MTk2NDE3NzQ3MjI5MDkyMA.G_B21Z.D2z22R90duqU5W_S98hFPyG9y0hpo8EDaLS-RQ', 
+    sahipID: '1206003845231149106',
   };
